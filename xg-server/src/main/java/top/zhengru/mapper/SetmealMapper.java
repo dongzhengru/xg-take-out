9b568465e683bd0388abda4933d4ac6657ee9260
董政儒 <zhengru05@126.com>
2023-07-13 13:40:28 +0800

    导入分类管理模块代码

---
+++ xg-server/src/main/java/top/zhengru/mapper/SetmealMapper.java
@@ -0,0 +1,17 @@
+package top.zhengru.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface SetmealMapper {
+
+    /**
+     * 根据分类id查询套餐的数量
+     * @param id
+     * @return
+     */
+    @Select("select count(id) from setmeal where category_id = #{categoryId}")
+    Integer countByCategoryId(Long id);
+
+}
