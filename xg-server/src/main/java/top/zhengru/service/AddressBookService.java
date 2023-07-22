0c26e6393866b3a2b826efef39a1414ac26dffe0
董政儒 <zhengru05@126.com>
2023-07-22 23:42:25 +0800

    导入地址簿模块代码

---
+++ xg-server/src/main/java/top/zhengru/service/AddressBookService.java
@@ -0,0 +1,20 @@
+package top.zhengru.service;
+
+import top.zhengru.entity.AddressBook;
+import java.util.List;
+
+public interface AddressBookService {
+
+    List<AddressBook> list(AddressBook addressBook);
+
+    void save(AddressBook addressBook);
+
+    AddressBook getById(Long id);
+
+    void update(AddressBook addressBook);
+
+    void setDefault(AddressBook addressBook);
+
+    void deleteById(Long id);
+
+}
