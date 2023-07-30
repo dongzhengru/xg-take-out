5a8d7b227a213fe8993a8768b13a17b520d07258
董政儒 <zhengru05@126.com>
2023-07-30 16:20:05 +0800

    来单提醒

--- xg-server/src/main/java/top/zhengru/service/impl/OrderServiceImpl.java
+++ xg-server/src/main/java/top/zhengru/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package top.zhengru.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -23,11 +24,14 @@ import top.zhengru.vo.OrderPaymentVO;
 import top.zhengru.vo.OrderStatisticsVO;
 import top.zhengru.vo.OrderSubmitVO;
 import top.zhengru.vo.OrderVO;
+import top.zhengru.websocket.WebSocketServer;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -51,6 +55,8 @@ public class OrderServiceImpl implements OrderService {
     private AddressBookMapper addressBookMapper;
     @Autowired
     private WeChatPayUtil weChatPayUtil;
+    @Autowired
+    private WebSocketServer webSocketServer;
 
 
     /**
@@ -157,6 +163,13 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         orderMapper.update(orders);
+
+        Map map = new HashMap();
+        map.put("type", 1);
+        map.put("order", ordersDB.getId());
+        map.put("content", "订单号" + outTradeNo);
+        String json = JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
     }
 
     /**
